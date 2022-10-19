c187e68d750b50877863633b300187ecfb911527
Gaurav Mahant <thegau22@gmail.com>
2022-10-18 22:36:29 -0600

    added standard input and detect for exit combo

--- play.js
+++ play.js
@@ -1,2 +1,21 @@
 const connect = require("./client");
 
+//setting up standard input to listen to userinput via keyboard
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+//creating a function to register data
+const handleUserInput = function (data) {
+  if (data === "\u0003") {
+    process.exit();
+  }
+};
+
+setupInput();
