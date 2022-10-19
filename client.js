f49b70aca0f000d0c5f49c6850a3ddb212991fc1
Gaurav Mahant <thegau22@gmail.com>
2022-10-18 22:16:21 -0600

    tested hardcoding directional moves to see if it works

--- client.js
+++ client.js
@@ -9,14 +9,21 @@ const connect = function () {
 
   conn.setEncoding("utf8");
 
+  //when receiving data performs action
   conn.on("data", (data) => {
     console.log("Message from server: ", data);
   })
 
+  //when connected performs actions
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-    conn.write("Name: GM")
+    conn.write("Name: GM");
   })
+
+  // conn.on("connect", () => {
+  //   conn.write("Move: down");
+  // })
+  //hard code to check if snake moves
 
   return conn;
 };
